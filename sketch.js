31a3b27d6869c0c6ebec9bbf51ff7129dfbd2ce8
tadashi <42453519+tadashi8@users.noreply.github.com>
2020-11-17 10:34:53 -1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,24 @@ let video;
 let poseNet;
 let pose;
 let skeleton;
-
+/*0	nose
+1	leftEye
+2	rightEye
+3	leftEar
+4	rightEar
+5	leftShoulder
+6	rightShoulder
+7	leftElbow
+8	rightElbow
+9	leftWrist
+10	rightWrist
+11	leftHip
+12	rightHip
+13	leftKnee
+14	rightKnee
+15	leftAnkle
+16	rightAnkle
+*/
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
